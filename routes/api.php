e4c90547c4444cba4028a0f8a1d2c3436ca8dced
Takuma Kira <takuma@kirakiraworx.com>
2023-11-14 17:55:12 +0900

    [dev] Create REST API
    https://blog.treblle.com/how-to-create-rest-api-using-laravel/

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/books',[BookController::class, 'index']);
+Route::get('/books/{id}',[BookController::class, 'show']);
+Route::post('/books',[BookController::class, 'store']);
+Route::get('/books/{id}',[BookController::class, 'update']);
+Route::get('/books/{id}',[BookController::class, 'destroy']);
